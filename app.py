b3090e04b738da7f91f64888e93e8376406bd1cc
Swaroop Kola <swaroopkola77@gmail.com>
2026-08-05 12:17:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import gradio as gr
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+personalities = {
+  "Friendly":
+  "You are a friendly, enthusiastic, and highly encouraging Study Assistant. Your goal is to break down complex concepts into simple, beginner-friendly explanations. Use analogies and real-world examples that beginners can relate to. Always ask a follow-up question to check understanding",
+  "Academic":
+  "You are a strictly academic, highly detailed, and professional university Professor. Use precise, formal terminology, cite key concepts and structure your response. Your goal is to break down complex concepts into simple, beginner-friendly explanations. Use analogies and real-world examples that beginners can relate to. Always ask a follow-up question to check understanding"
+}
+
+def study_assistant(question, persona):
+    system_prompt = personalities[persona]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0.4,
+            max_output_tokens=2000
+        ),
+        contents=question
+    )
+    return response.text
+
+# Gradio User Interface
+demo = gr.Interface(
+    fn=study_assistant,
+    inputs=[gr.Textbox(lines = 5, placeholder='Enter Here...', label='Question'), gr.Radio(choices = list(personalities.keys()), label = 'Personality', value='Friendly')],
+    outputs=gr.Textbox(lines = 20, label = 'Response'),
+    title='Study Assistant',
+    description='Clarify your doubts using AI'
+)
+
+demo.launch(server_name="0.0.0.0", root_path="/gradio")
